b54fc4a58ac6dca6e2eee07a4ac432d72e54ed99
Ghozti <62896836+ghozti@users.noreply.github.com>
2022-11-25 21:25:24 -0500

    - messed with cam

--- desktop/src/com/ghozti/game/desktop/DesktopLauncher.java
+++ desktop/src/com/ghozti/game/desktop/DesktopLauncher.java
@@ -7,6 +7,12 @@ import com.ghozti.game.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.foregroundFPS = 120;
+		config.backgroundFPS = 0;
+		config.title = "Dead City";
+		config.width = 1900;
+		config.height = 1070;
+		config.resizable = true;
 		new LwjglApplication(new Game(), config);
 	}
 }
