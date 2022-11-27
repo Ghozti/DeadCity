ef40b52c42651f451fd0cbf0619bc67c301c1b53
Ghozti <62896836+ghozti@users.noreply.github.com>
2022-11-26 21:04:37 -0500

    - got cam to move

--- core/src/com/ghozti/game/launcher/Launcher.java
+++ core/src/com/ghozti/game/launcher/Launcher.java
@@ -13,6 +13,8 @@ import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import org.w3c.dom.Text;
 
 public class Launcher implements Screen {
@@ -20,6 +22,7 @@ public class Launcher implements Screen {
     Batch batch;
     Texture texture;
     OrthographicCamera cam;
+    Viewport viewPort;
     Sprite mapSprite;
     float rotationSpeed;
 
@@ -30,9 +33,8 @@ public class Launcher implements Screen {
         mapSprite = new Sprite(new Texture(Gdx.files.internal("testMap.png")));
         mapSprite.setPosition(0, 0);
         mapSprite.setSize(374, 328);
-        cam = new OrthographicCamera(60, 60 * (1080f / 1920));
-        cam.position.set(cam.viewportWidth / 2f, cam.viewportHeight / 2f, 0);
-        cam.update();
+        cam = new OrthographicCamera();
+        viewPort = new StretchViewport(25,25,cam);
     }
 
     public void update(){
@@ -40,65 +42,35 @@ public class Launcher implements Screen {
     }
 
     private void handleInput() {
-        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-            cam.zoom += 0.02;
-            //If the A Key is pressed, add 0.02 to the Camera's Zoom
+        if (Gdx.input.isKeyPressed(Input.Keys.W)){
+            cam.position.y += 10;
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.Q)) {
-            cam.zoom -= 0.02;
-            //If the Q Key is pressed, subtract 0.02 from the Camera's Zoom
+        if (Gdx.input.isKeyPressed(Input.Keys.A)){
+            cam.position.x -= 10;
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            cam.translate(-3, 0, 0);
-            //If the LEFT Key is pressed, translate the camera -3 units in the X-Axis
+        if (Gdx.input.isKeyPressed(Input.Keys.S)){
+            cam.position.y -= 10;
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            cam.translate(3, 0, 0);
-            //If the RIGHT Key is pressed, translate the camera 3 units in the X-Axis
+        if (Gdx.input.isKeyPressed(Input.Keys.D)){
+            cam.position.x += 10;
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            cam.translate(0, -3, 0);
-            //If the DOWN Key is pressed, translate the camera -3 units in the Y-Axis
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            cam.translate(0, 3, 0);
-            //If the UP Key is pressed, translate the camera 3 units in the Y-Axis
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-            cam.rotate(-rotationSpeed, 0, 0, 1);
-            //If the W Key is pressed, rotate the camera by -rotationSpeed around the Z-Axis
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.E)) {
-            cam.rotate(rotationSpeed, 0, 0, 1);
-            //If the E Key is pressed, rotate the camera by rotationSpeed around the Z-Axis
-        }
-
-        cam.zoom = MathUtils.clamp(cam.zoom, 0.1f, 100/cam.viewportWidth);
-
-        float effectiveViewportWidth = cam.viewportWidth * cam.zoom;
-        float effectiveViewportHeight = cam.viewportHeight * cam.zoom;
-
-        cam.position.x = MathUtils.clamp(cam.position.x, effectiveViewportWidth / 2f, 100 - effectiveViewportWidth / 2f);
-        cam.position.y = MathUtils.clamp(cam.position.y, effectiveViewportHeight / 2f, 100 - effectiveViewportHeight / 2f);
+        cam.update();
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         update();
-        cam.update();
-        batch.setProjectionMatrix(cam.combined);
-
         batch.begin();
+        batch.setProjectionMatrix(cam.combined);
         mapSprite.draw(batch);
         batch.end();
     }
 
     @Override
     public void resize(int width, int height) {
-        cam.viewportWidth = 30f;
-        cam.viewportHeight = 30f * height/width;
-        cam.update();
+        viewPort.update(width,height,true);//tells the viewport to update accordingly
+        batch.setProjectionMatrix(cam.combined);
     }
 
     @Override
