9ea5b72f85e1ea3aaf5a3ecef99b019e56cf6659
Ghozti <62896836+ghozti@users.noreply.github.com>
2022-12-14 21:02:22 -0500

    - worked on Entity

--- core/src/com/ghozti/game/backend/entities/Entity.java
+++ core/src/com/ghozti/game/backend/entities/Entity.java
@@ -2,18 +2,23 @@ package com.ghozti.game.backend.entities;
 
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 public abstract class Entity {
 
     Texture texture;//change to textureRegion
-    Rectangle hitbox;
     Vector2 position;
+    float width, height;
+    Batch batch;
 
-    public Entity(Texture texture, float startX, float startY){
+    public Entity(Texture texture, float startX, float startY, float width, float height, Batch batch){
         this.texture = texture;
         position = new Vector2(startX,startY);
+        this.width = width;
+        this.height = height;
+        this.batch = batch;
     }
 
     public Texture getTexture(){
@@ -32,6 +37,10 @@ public abstract class Entity {
         return position;
     }
 
+    public Batch getBatch(){
+        return batch;
+    }
+
     public void setTexture(Texture texture){
         this.texture = texture;
     }
@@ -42,5 +51,9 @@ public abstract class Entity {
 
     public void teleport(float x, float y){
         position.set(x,y);
+    }
+
+    public void draw(){
+        batch.draw(texture,position.x,position.y);
     }
 }
