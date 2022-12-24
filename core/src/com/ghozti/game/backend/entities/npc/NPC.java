e619db06ea4acd05a2a8626d7c7ee0467e7a1f96
Ghozti <62896836+ghozti@users.noreply.github.com>
2022-12-23 20:08:19 -0500

    - added files

--- core/src/com/ghozti/game/backend/entities/npc/NPC.java
+++ core/src/com/ghozti/game/backend/entities/npc/NPC.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.ghozti.game.backend.entities.Entity;
 
 public class NPC extends Entity {
+
     public NPC(Texture texture, float startX, float startY, float width, float height, Batch batch) {
         super(texture, startX, startY, width, height, batch);
     }
